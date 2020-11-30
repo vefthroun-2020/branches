7fae6e33783e0d7e99f1b5ca963abedcaa29a886
Smari <ellertsmari@gmail.com>
2020-11-30 13:35:30 +0000

    I just made a new component to this project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Smari from './components/Smari';
 import Testing from './components/Testing';
+import MyName from './components/MyName';
 
 function App() {
   return (
     <div>
       <Smari />
       <Testing />
+      <MyName />
     </div>
   );
 }
---
+++ src/components/MyName.js
@@ -0,0 +1,9 @@
+const Smari = ()=>{
+    return(
+        <div>
+            <h1>chagne this into something</h1>
+            <p>change this too</p>
+        </div>
+    )
+}
+export default Smari;
