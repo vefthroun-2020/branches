b29d56c7614e4132819175c0961adba0128ee1b6
Aridaniel <aridagnars@gmail.com>
2020-11-30 13:34:54 +0000

    ari cado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div>
       <Smari />
+    </div>,
+    <div>
+      <Ari />
     </div>
   );
 }
---
+++ src/components/Avacadoman.js
@@ -0,0 +1,9 @@
+const Ari = ()=>{
+    return(
+        <div>
+            <h1>AVACADO_MAN</h1>
+            <p>look how cool it is</p>
+        </div>
+    )
+}
+export default Ari;
