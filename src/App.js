521356f6f2719c370a98f39a3a16cf1fb30cc306
Paula Nerenberg <paula1@ymail.com>
2020-11-30 13:44:15 +0000

    My commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Smari from './components/Smari';
 import Testing from './components/Testing';
+import Paula from './components/Paula';
 
 function App() {
   return (
     <div>
       <Smari />
       <Testing />
+      <Paula/>
     </div>
   );
 }
---
+++ src/components/Paula.js
@@ -0,0 +1,9 @@
+const Paula = ()=>{
+    return(
+        <div>
+            <h1>This is Paulas coponent</h1>
+            <p>look how cool it is</p>
+        </div>
+    )
+}
+export default Paula;
