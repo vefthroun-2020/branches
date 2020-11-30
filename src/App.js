2c975ab54e6b23f3d4af3a9f4cabb72963f4f655
Daniel Bergmann <dba222@outlook.com>
2020-11-30 13:35:45 +0000

    added a gif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Smari from './components/Smari';
+import Daniel from './components/Daniel';
 
 function App() {
   return (
     <div>
       <Smari />
+      <Daniel />
     </div>
   );
 }
