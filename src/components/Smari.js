7abf64f4a22eeb67557f022587feaf4aa5d9d529
Smari <ellertsmari@gmail.com>
2020-11-30 12:51:50 +0000

    branches test

---
+++ src/components/Smari.js
@@ -0,0 +1,9 @@
+const Smari = ()=>{
+    return(
+        <div>
+            <h1>This is my own Smári coponent</h1>
+            <p>look how cool it is</p>
+        </div>
+    )
+}
+export default Smari;
