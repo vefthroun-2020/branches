74715d8f94eac387bce767aa81a8edcf5d530147
Smari <ellertsmari@gmail.com>
2020-11-30 14:53:11 +0000

    reverted back to before the gif

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Smari from './components/Smari';
+<<<<<<< HEAD
 import Testing from './components/Testing';
 import Daniel from './components/Daniel';
+=======
+>>>>>>> parent of facb395 (added a testing component)
 
 function App() {
   return (
     <div>
       <Smari />
+<<<<<<< HEAD
       <Testing />
       <Daniel />
+=======
+>>>>>>> parent of facb395 (added a testing component)
     </div>
   );
 }
--- src/components/Testing.js
+++
@@ -1,9 +0,0 @@
-const Testing = ()=>{
-    return(
-        <div>
-            <h1>This is a test</h1>
-            <p>one two... one two....</p>
-        </div>
-    )
-}
-export default Testing;
