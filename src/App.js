0f21673a35c63082ac24dce7e6a44b91d11d38e7
IrisElva <iriselva@gmail.com>
2020-11-30 13:35:44 +0000

    Iris component to this project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Smari from './components/Smari';
+import Iris from './components/Iris';
 
 function App() {
   return (
     <div>
       <Smari />
+      <Iris />
     </div>
   );
 }
---
+++ src/components/Iris.js
@@ -0,0 +1,9 @@
+const Iris = ()=>{
+    return(
+        <div>
+            <h1>This is my Iris coponent</h1>
+            <p>hello!</p>
+        </div>
+    )
+}
+export default Iris;
